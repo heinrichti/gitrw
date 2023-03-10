f3c8a3ff943ba6671f2b454108f5324fc52de75e
Tia Heinrich <2929650+heinrichti@users.noreply.github.com>
2023-03-10 16:20:12 +0100

    prepared commandline arguments

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::path::Path;
 
-use clap::Parser;
+use clap::{Parser, Subcommand, ArgGroup};
 use mimalloc::MiMalloc;
 
 #[global_allocator]
@@ -8,11 +8,83 @@ static GLOBAL: MiMalloc = MiMalloc;
 
 #[derive(clap::Parser)]
 struct Cli {
-    repository: String,
+    repository: Option<String>,
+
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Contributor related actions like list and rewrite
+    #[command(subcommand)]
+    Contributor(ContributorArgs),
+
+    /// Remove files and whole directories from the repository 
+    #[command(group(ArgGroup::new("input")
+                        .required(true)
+                        .multiple(true)))]
+    Remove {
+        /// File to remove. Argument can be specified multiple times
+        #[arg(short, long, group = "input")]
+        file: Option<Vec<String>>,
+
+        /// Directory to remove. Argument can be specified multiple times
+        #[arg(short, long, group = "input")]
+        directory: Option<Vec<String>>
+    },
+
+    /// Remove empty commits that are no merge commits
+    PruneEmpty
+}
+
+#[derive(Subcommand)]
+enum ContributorArgs {
+    /// Lists all authors and committers
+    List,
+    /// Allows to rewrite contributors 
+    Rewrite {
+        /// Format inside file: Old User <old@user.mail> = New User <new@user.mail>
+        mapping_file: String
+    }
 }
 
 fn main() {
     let cli = Cli::parse();
-    let repository_path = Path::new(&cli.repository);
-    gitrw::list_contributors(repository_path).unwrap();
+    let repository_path = 
+        if let Some(repository_path) = &cli.repository {
+            Path::new(repository_path)
+        } else {
+            Path::new(".")
+        };
+
+    match cli.command {
+        Commands::Contributor(args) => match args {
+            ContributorArgs::List => gitrw::list_contributors(repository_path).unwrap(), 
+            ContributorArgs::Rewrite { mapping_file: file } => {
+                println!("rewrite from file: {file}");
+                todo!();
+            }
+        },
+        Commands::Remove { file, directory } => {
+            if let Some(dir) = directory {
+                for d in dir {
+                    println!("Deleting directory: {d}");
+                }
+            }
+
+            if let Some(file) = file {
+                for f in file {
+                    println!("Deleting file: {f}");
+                }
+            }
+
+            todo!();
+        },
+
+        Commands::PruneEmpty => {
+            println!("Pruning empty commits");
+            todo!();
+        }
+    };
 }
