f9df15605849792bc799f8951bd10bc6aac1d4f3
Tia Heinrich <2929650+heinrichti@users.noreply.github.com>
2023-07-20 22:15:34 +0200

    simplified remove stuff

--- src/remove.rs
+++ src/remove.rs
@@ -7,37 +7,10 @@ use libgitrw::{
 };
 use rustc_hash::FxHashMap;
 
-fn or<'a, T1, F1, F2>(f: F1, g: F2) -> Box<dyn Fn(T1, T1) -> bool + 'a>
-where
-    T1: Copy,
-    F1: Fn(T1, T1) -> bool + 'a,
-    F2: Fn(T1, T1) -> bool + 'a,
-{
-    Box::new(move |path, filename| f(path, filename) || g(path, filename))
-}
-
-fn or_folder<'a, T1, F1, F2>(f: F1, g: F2) -> Box<dyn Fn(T1) -> bool + 'a>
-where
-    T1: Copy,
-    F1: Fn(T1) -> bool + 'a,
-    F2: Fn(T1) -> bool + 'a,
-{
-    Box::new(move |path| f(path) || g(path))
-}
-
-fn single<'a, T1, F1>(f: F1) -> Box<dyn Fn(T1, T1) -> bool + 'a>
-where
-    T1: Copy,
-    F1: Fn(T1, T1) -> bool + 'a,
-{
-    Box::new(f)
-}
-
-fn single_folder<'a, T1, F1>(f: F1) -> Box<dyn Fn(T1) -> bool + 'a>
-where
-    F1: Fn(T1) -> bool + 'a,
-{
-    Box::new(f)
+macro_rules! b {
+    ( $x:expr ) => {
+        Box::new($x)
+    };
 }
 
 fn last_index_of(path: &[u8], needle: u8) -> Option<usize> {
@@ -49,32 +22,33 @@ fn last_index_of(path: &[u8], needle: u8) -> Option<usize> {
     None
 }
 
-fn build_folder_delete_patterns<'a>(folders: &'a [String]) -> Box<dyn Fn(&'a [u8]) -> bool + 'a> {
-    let mut delete_folder = single_folder(|_path: &[u8]| false);
+type DynFn<'a> = Box<dyn Fn(&'a [u8]) -> bool + 'a>;
+type DynFn2<'a> = Box<dyn Fn(&[u8], &[u8]) -> bool + 'a>;
+
+fn build_folder_delete_patterns(folders: &[String]) -> DynFn {
+    let mut delete_folder: DynFn = Box::new(|_path| false);
 
     for folder in folders.iter().map(|f| f.as_bytes()) {
         if folder[0] == b'*' {
             if folder[folder.len() - 1] == b'/' {
-                delete_folder = or_folder(delete_folder, |path| path.ends_with(&folder[1..]));
+                delete_folder = b!(move |path| delete_folder(path) || path.ends_with(&folder[1..]));
             } else {
                 // handles trailing slash
-                delete_folder = or_folder(delete_folder, |path| {
-                    path[0..path.len() - 1].ends_with(&folder[1..])
-                });
+                delete_folder = b!(move |path| delete_folder(path)
+                    || path[0..path.len() - 1].ends_with(&folder[1..]));
             }
         } else if folder[folder.len() - 1] == b'*' {
-            delete_folder = or_folder(delete_folder, |path| {
-                path.starts_with(&folder[0..folder.len() - 1])
-            })
+            delete_folder =
+                b!(move |path| delete_folder(path)
+                    || path.starts_with(&folder[0..folder.len() - 1]));
         } else if folder[0] == b'/' {
             // absolute path, no wildcard
             if folder[folder.len() - 1] == b'/' {
-                delete_folder = or_folder(delete_folder, |path| path.eq(folder));
+                delete_folder = b!(move |path| delete_folder(path) || path.eq(folder));
             } else {
                 // handles missing trailing slash
-                delete_folder = or_folder(delete_folder, |path| {
-                    path.len() == folder.len() + 1 && path[0..path.len() - 1].eq(folder)
-                });
+                delete_folder = b!(move |path| delete_folder(path)
+                    || path.len() == folder.len() + 1 && path[0..path.len() - 1].eq(folder));
             }
         } else {
             // relative path, no wildcard
@@ -86,75 +60,69 @@ fn build_folder_delete_patterns<'a>(folders: &'a [String]) -> Box<dyn Fn(&'a [u8
                 folder.insert(0, b'/');
             }
 
-            delete_folder = or_folder(delete_folder, move |path| path.ends_with(&folder));
+            delete_folder = b!(move |path| delete_folder(path) || path.ends_with(&folder));
         }
     }
 
     delete_folder
 }
 
-fn build_file_delete_patterns<'a>(
-    files: &'a [String],
-) -> Box<dyn Fn(&'a [u8], &'a [u8]) -> bool + 'a> {
-    let mut delete_file = single(|_path: &[u8], _filename: &[u8]| false);
+fn build_file_delete_patterns(files: &[String]) -> DynFn2 {
+    let mut delete_file: DynFn2 = b!(|_path, _filename| false);
     for file in files.iter().map(|f| f.as_bytes()) {
         if file[0] == b'*' {
             match last_index_of(file, b'/') {
                 // */bin/test.txt
                 Some(last_slash) => {
-                    delete_file = or(delete_file, move |path, filename| {
-                        path.ends_with(&file[1..last_slash + 1])
-                            && filename.eq(&file[last_slash + 1..])
-                    })
+                    delete_file = b!(move |path, filename| delete_file(path, filename)
+                        || (path.ends_with(&file[1..last_slash + 1])
+                            && filename.eq(&file[last_slash + 1..])));
                 }
                 // *mytest.txt
                 None => {
-                    delete_file = or(delete_file, |_path, filename| {
-                        filename.ends_with(&file[1..])
-                    })
+                    delete_file = b!(move |path, filename| delete_file(path, filename)
+                        || filename.ends_with(&file[1..]));
                 }
             }
         } else if file[file.len() - 1] == b'*' {
             match last_index_of(file, b'/') {
                 // /some/folder/file_to_delete*
                 Some(last_slash) => {
-                    delete_file = or(delete_file, move |path, filename| {
-                        path.eq(&file[0..last_slash + 1])
-                            && filename.starts_with(&file[last_slash + 1..file.len() - 1])
-                    })
+                    delete_file = b!(move |path, filename| delete_file(path, filename)
+                        || (path.eq(&file[0..last_slash + 1])
+                            && filename.starts_with(&file[last_slash + 1..file.len() - 1])));
                 }
                 // file_to_delete*
                 None => {
-                    delete_file = or(delete_file, move |_path, filename| {
-                        filename.starts_with(&file[0..file.len() - 1])
-                    })
+                    delete_file = b!(move |path, filename| delete_file(path, filename)
+                        || filename.starts_with(&file[0..file.len() - 1]));
                 }
             }
         } else if file[0] == b'/' {
             // absolute path: /some/folder/file_to_delete.txt
             let last_slash = last_index_of(file, b'/').unwrap();
-            delete_file = or(delete_file, move |path, filename| {
-                path.len() + filename.len() == file.len()
+            delete_file = b!(move |path, filename| delete_file(path, filename)
+                || (path.len() + filename.len() == file.len()
                     && path.eq(&file[0..last_slash + 1])
-                    && filename.eq(&file[last_slash + 1..])
-            });
+                    && filename.eq(&file[last_slash + 1..])));
         } else {
             // simple file name, should not contain any slashes: file_to_delete.txt
             if last_index_of(file, b'/').is_some() {
                 panic!("Unknown pattern: {}", file.as_bstr());
             }
 
-            delete_file = or(delete_file, move |_path, filename| filename.eq(file));
+            delete_file =
+                b!(move |path, filename| delete_file(path, filename) || filename.eq(file));
         }
     }
 
     delete_file
 }
 
-fn update_tree<'a>(
+fn update_tree(
     _tree_hash: &TreeHash,
-    should_delete_file: &(dyn Fn(&'a [u8], &'a [u8]) -> bool),
-    should_delete_folder: &(dyn Fn(&'a [u8]) -> bool),
+    should_delete_file: &DynFn2,
+    should_delete_folder: &DynFn,
 ) -> Option<TreeHash> {
     if should_delete_file(b"", b"") {}
     if should_delete_folder(b"") {}
